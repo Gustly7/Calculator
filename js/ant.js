08df07d51bad20b1a375dd166e45354031450e5d
Gustly <Gustly@yandex.ru>
2018-07-12 13:23:47 +0400

    Patterns

---
+++ js/ant.js
@@ -0,0 +1,44 @@
+function ant () {
+  return {
+    x: 0,
+    y: 0,
+    angle: 0,
+    init: function () {
+      $('body').append('<div id="ant">🐜</div>');
+      window.ant = this;
+    },
+    nextStep: function () {
+      console.log('nextStep')
+      var newX = Math.round(Math.random() * (window.innerWidth - 50));
+      var newY = Math.round(Math.random() * (window.innerHeight - 50));
+      this.angle = (Math.atan2((newY - this.y), (newX - this.x)) + Math.PI / 2) / Math.PI * 180;
+
+
+      $('#ant').animate({ border: this.angle }, {
+        step: function(now,fx) {
+          //$(this).css('-webkit-transform','rotate('+now+'deg)');
+          $('#ant').css('transform','rotate('+now+'deg)');
+        },
+        duration:1000
+      },'linear');
+      //$('#ant').css('transform','rotate('+this.angle+'deg)');
+
+      //$('#ant').animate({transform: 'rotate(' + this.angle + 'deg)'}, 500);
+      this.x = newX;
+      this.y = newY;
+      var thisAnt = this;
+      $('#ant').animate({top: newY + "px", left: newX + 'px'}
+        , {
+          duration: 2000,
+          specialEasing: {
+            width: "easeOutBounce",
+            height: "easeOutBounce"
+          }
+          , complete: function () {
+            thisAnt.nextStep();
+          }
+        }
+      );
+    }
+  }
+}
--- js/calculator.js
+++ js/calculator.js
@@ -1,138 +1,163 @@
 var keys = document.querySelectorAll('#calculator span.calc-btn');
-var screen = document.querySelector('.screen'); //Инпут основного экрана
+//var screen = document.querySelector('.screen'); //Инпут основного экрана
 var operand1 = 0, operand2 = 0; //Первый и второй операнды
 var newOperand = true;          //Флаг того что сейчас пойде новый операнд
 var operation = '';             //Операция
 
-//Добавление числа
-function addNumber (pNum) {
-  //Если новый операнд то чистим экран
-  if (newOperand) {
-    screen.innerHTML = '';
-    newOperand = false;
-  }
-  screen.innerHTML += pNum;
-}
-
-//Добавление дробной части
-function addDot () {
-  if (screen.innerHTML.indexOf('.') == - 1)
-    screen.innerHTML += '.';
-}
-
-//backspace
-function backspace () {
-  screen.innerHTML = screen.innerHTML.substr(0,screen.innerHTML.length-1);
-}
-
-//Очищение экрана
-function clearSreen () {
-  screen.innerHTML = '';
-  operation = ''
-}
-
-//Считаем процент
-function procentEval(){
-  screen.innerHTML = operand1/100*parseFloat(screen.innerHTML);
-}
-
-//Считаем
-function mathEval () {
-  //При первом нажатие "=" сохраняем 2й операнд
-  if (!newOperand) {
-    operand2 = parseFloat(screen.innerHTML);
-    newOperand = true;
-  }
-  //Список операция для подсчета
-  if (operation != '') {
-    switch (operation) {
-      case 'plus':
-        operand1 += operand2;
-        break;
-      case 'minus':
-        operand1 -= operand2;
-        break;
-      case 'mult':
-        operand1 *= operand2;
-        break;
-      case 'division':
-        operand1 /= operand2;
-        break;
-      case 'sqrt':
-        operand1 = Math.sqrt(operand1);
-        break;
-      case 'invert2':
-        operand1 = 1/operand1;
-        break;
-      case 'invert':
-        operand1 *= -1;
-        break;
+function calculator () {
+  return {
+    screen: ''
+    ,
+    //Добавление числа
+    addNumber: function (pNum) {
+      //Если новый операнд то чистим экран
+      if (newOperand) {
+        window.z1 = this;
+        this.screen[0].innerHTML = '';
+        newOperand = false;
+      }
+      console.log('test2');
+      this.screen[0].innerHTML += pNum;
     }
-    screen.innerHTML = operand1;
-  }
-}
-
-//Сохраняем имя операции и 1й операнд
-function mathOperation (pOper) {
-  newOperand = true;
-  operation = pOper;
-  operand1 = parseFloat(screen.innerHTML);
-}
-
-//Парсим нажатую клавишу
-function parseKey (pOper) {
-  switch (true) {
-    //Числа
-    case /^[0-9]{1}$/.test(pOper):
-      addNumber(pOper);
-      break;
+    ,
+    //Добавление дробной части
+    addDot: function () {
+      if (this.screen[0].innerHTML.indexOf('.') == -1)
+        this.screen[0].innerHTML += '.';
+    }
+    ,
     //backspace
-    case pOper == 'backspace':
-      backspace();
-      break;
-    //Точка
-    case pOper == 'dot':
-      addDot();
-      break;
+    backspace: function () {
+      this.screen[0].innerHTML = this.screen[0].innerHTML.substr(0, this.screen[0].innerHTML.length - 1);
+    }
+    ,
     //Очищение экрана
-    case pOper == 'clear':
-      clearSreen();
-      break;
-    //Операции которые считаются не сразу
-    case ['plus', 'minus', 'mult', 'division'].indexOf(pOper) > -1:
-      mathOperation(pOper);
-      break;
-    //Равно
-    case pOper == 'eval':
-      mathEval();
-      break;
-    //Процент
-    case pOper == 'procent':
-      procentEval();
-      break;
-    //Здесь операции которые считаются сразу же: корень, 1/x, +-
-    case ['sqrt','invert2','invert'].indexOf(pOper) > -1:
-      mathOperation(pOper);
-      mathEval();
-      break;
+    clearSreen: function () {
+      this.screen[0].innerHTML = '';
+      operation = ''
+    }
+    ,
+    //Считаем процент
+    procentEval: function () {
+      this.screen[0].innerHTML = operand1 / 100 * parseFloat(this.screen[0].innerHTML);
+    }
+    ,
+    //Считаем
+    mathEval: function () {
+      //При первом нажатие "=" сохраняем 2й операнд
+      if (!newOperand) {
+        operand2 = parseFloat(this.screen[0].innerHTML);
+        newOperand = true;
+      }
+      //Список операция для подсчета
+      if (operation != '') {
+        switch (operation) {
+          case 'plus':
+            operand1 += operand2;
+            break;
+          case 'minus':
+            operand1 -= operand2;
+            break;
+          case 'mult':
+            operand1 *= operand2;
+            break;
+          case 'division':
+            operand1 /= operand2;
+            break;
+          case 'sqrt':
+            operand1 = Math.sqrt(operand1);
+            break;
+          case 'invert2':
+            operand1 = 1 / operand1;
+            break;
+          case 'invert':
+            operand1 *= -1;
+            break;
+        }
+        this.screen[0].innerHTML = operand1;
+      }
+    }
+    ,
+    //Сохраняем имя операции и 1й операнд
+    mathOperation: function (pOper) {
+      newOperand = true;
+      operation = pOper;
+      operand1 = parseFloat(this.screen[0].innerHTML);
+    }
+    ,
+    //Парсим нажатую клавишу
+    parseKey: function (pOper) {
+      switch (true) {
+        //Числа
+        case /^[0-9]{1}$/.test(pOper):
+          this.addNumber(pOper);
+          break;
+        //backspace
+        case pOper == 'backspace':
+          this.backspace();
+          break;
+        //Точка
+        case pOper == 'dot':
+          this.addDot();
+          break;
+        //Очищение экрана
+        case pOper == 'clear':
+          this.clearSreen();
+          break;
+        //Операции которые считаются не сразу
+        case ['plus', 'minus', 'mult', 'division'].indexOf(pOper) > -1:
+          this.mathOperation(pOper);
+          break;
+        //Равно
+        case pOper == 'eval':
+          this.mathEval();
+          break;
+        //Процент
+        case pOper == 'procent':
+          this.procentEval();
+          break;
+        //Здесь операции которые считаются сразу же: корень, 1/x, +-
+        case ['sqrt', 'invert2', 'invert'].indexOf(pOper) > -1:
+          this.mathOperation(pOper);
+          this.mathEval();
+          break;
+      }
+    }
+    ,
+    init: function () {
+      for (var i = 0; i < keys.length; i++) {
+        $(keys[i]).on('click', function (e) {
+          var oper = $(e.target).data('oper');
+          parseKey(oper);
+        });
+      }
+
+      $('.calculator').each(function (num, cElem) {
+        //В каждый элемент с классом calculator добавляем объект калькулятор в виде атрибута
+        cElem.calculator = calculator();
+        //Ищем экран
+        cElem.calculator.screen = $(cElem).find('.screen:first');
+        //Цикл, вешаем функцию на клик кнопок
+        $(cElem).find('.calc-btn').each(function (num, bElem) {
+          //На каждую кнопку вешаем ссылку на наш объект калькулятора
+          bElem.calculator = cElem.calculator;
+          $(bElem).on('click', function (e) {
+            var oper = $(e.target).data('oper');
+            //По клику запускаем parseKey соответствующего калькулятора
+            this.calculator.parseKey(oper);
+          });
+        })
+      })
+    }
   }
 }
 
-function init() {
-  for (var i = 0; i < keys.length; i++) {
-    $(keys[i]).on('click', function (e) {
-      var oper = $(e.target).data('oper');
-      parseKey(oper);
-    });
-  }
+$(document).on('keydown', function (e) {
+  //console.log(e.keyCode);
+})
 
-  $(document).on('keydown', function (e) {
-    //console.log(e.keyCode);
-  })
-
-  $(document).on('keyup', function (e) {
-    console.log(e.keyCode);
-    $('.calcKey-' + e.keyCode).click().addClass('span-active');
-    setTimeout(function () { $('.calcKey-' + e.keyCode).removeClass('span-active') }, 300)
-  })
-}
+$(document).on('keyup', function (e) {
+  console.log(e.keyCode);
+  $('.calcKey-' + e.keyCode).click().addClass('span-active');
+  setTimeout(function () { $('.calcKey-' + e.keyCode).removeClass('span-active') }, 300)
+})
